5b4ee17d5314056bdee14d12e5edcf9a103a7950
purplebeam <konvpapageorgiou@gmail.com>
2023-06-05 15:23:21 +0300

    fetching my noob practice

---
+++ Exercisim.org - My Solutions/JavaScript/1_HelloWorld/HelloWorld.js
@@ -0,0 +1,24 @@
+/*
+
+Instructions
+The classical introductory exercise. Just say "Hello, World!".
+
+"Hello, World!" is the traditional first program for beginning programming in a new language or environment.
+
+The objectives are simple:
+
+1) Write a function that returns the string "Hello, World!".
+2) Run the test suite and make sure that it succeeds.
+3) Submit your solution and check it at the website.
+4) If everything goes well, you will be ready to fetch your first real exercise.
+
+Source: https://exercism.org/tracks/javascript/exercises/hello-world
+
+This is only a SKELETON file for the 'Hello World' exercise.
+It is used only to solve the exercise in the platform exercism.org
+
+*/
+
+export function hello() {
+    return 'Hello, World!';
+  }
---
+++ Exercisim.org - My Solutions/PHP/HelloWorld.php
@@ -0,0 +1,29 @@
+<!-- Instructions
+The classical introductory exercise. Just say "Hello, World!".
+
+"Hello, World!" is the traditional first program for beginning programming in a new language or environment.
+
+The objectives are simple:
+
+1) Write a function that returns the string "Hello, World!".
+2) Run the test suite and make sure that it succeeds.
+3) Submit your solution and check it at the website.
+4) If everything goes well, you will be ready to fetch your first real exercise. 
+
+source: https://exercism.org/tracks/php/exercises/hello-world
+
+
+-->
+
+<?php
+
+function helloWorld()
+{
+    return "Hello, World!";
+}
+
+
+$HelloW = HelloWorld();
+echo $HelloW;
+
+?>
