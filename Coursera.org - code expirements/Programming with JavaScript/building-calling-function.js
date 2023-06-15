00468267c3960bad1388d068e14c732a919e1b1f
purplebeam <konvpapageorgiou@gmail.com>
2023-06-16 00:17:38 +0300

    completed

---
+++ Coursera.org - code expirements/Programming with JavaScript/building-calling-function.js
@@ -0,0 +1,11 @@
+// function listArrayItems(arr) {
+//     for (var i = 0; i < arr.length; i++){
+//         if (arr[i] == 'red'){
+//         console.log(i*100,"tomato!")    
+//         }
+//         else { 
+//             console.log(i*100, arr[i])
+//          }
+        
+//     }
+// }
